827a58cfd203d6463566aa3427fe3f0c6a2164fd
garvit <agarvit1142000@gmail.com>
2022-07-19 09:40:48 +0530

    Added socket programming

---
+++ src/First.js
@@ -0,0 +1,31 @@
+import io from "socket.io-client";
+import { useState, useEffect } from "react";
+const socket = io("http://localhost:3001");
+
+const First = () => {
+  const [message, setMessage] = useState("");
+  socket.on("Welcome", (data) => {
+    alert(data);
+  });
+  socket.on("receiveMessage", (data) => {
+    alert(data);
+  });
+  return (
+    <div>
+      <input
+        type="text"
+        value={message}
+        onChange={(e) => setMessage(e.target.value)}
+      />
+      <br></br>
+      <button
+        onClick={() => {
+          socket.emit("sendMessage", message);
+        }}
+      >
+        Send
+      </button>
+    </div>
+  );
+};
+export default First;
